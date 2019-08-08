b15fe386934939510f5a25456192847e7eac07d0
Johnathan McRoberts <jmcroberts@actify.com>
2019-08-08 14:12:08 +0100

    Add songs file parser + tester

---
+++ SongsWebApp/SongsFileImportExport/Parser/SongFileParser.cs
@@ -0,0 +1,236 @@
+﻿using System.Linq;
+
+namespace SongsFileImportExport.Parser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    using SongsDatabase.DataModels;
+
+    public enum FileReadState
+    {
+        Reset,
+        OnDate,
+        OnLocation,
+        OnArtist
+    };
+
+    public static class SongFileParser
+    {
+        #region Constants
+
+        public const int NumberWordsInDate = 4;
+        public static readonly string[] DayStrings = 
+            {"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"};
+
+        public static readonly string[] OrdinalStrings =
+            {"st", "nd", "rd", "th"};
+
+
+        public static readonly char[] IgnoreChars = {' ', '\t'};
+
+        public static readonly Dictionary<string, int> MonthStringsToNumbers = 
+            new Dictionary<string, int>
+            {
+                {"january", 1},
+                {"february", 2},
+                {"march", 3},
+
+                {"april", 4},
+                {"may", 5},
+                {"june", 6},
+
+                {"july", 7},
+                {"august", 8},
+                {"september", 9},
+
+                {"october", 10},
+                {"november", 11},
+                {"december", 12}
+            };
+
+        #endregion
+
+        #region Private Functions
+
+        private static void ProcessLine(
+            string line,
+            ref FileReadState state,
+            ref List<AlbumPlayed> albums,
+            ref AlbumPlayed currentAlbumPlayed)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+                return;
+
+            switch (state)
+            {
+                case FileReadState.Reset:
+                    ParseLineWhenReset(line, ref state, ref currentAlbumPlayed);
+                    break;
+                case FileReadState.OnDate:
+                    ParseLineWhenOnDate(line, ref state, ref currentAlbumPlayed);
+                    break;
+                case FileReadState.OnLocation:
+                    ParseLineWhenOnLocation(line, ref state, ref currentAlbumPlayed);
+                    break;
+                case FileReadState.OnArtist:
+                    ParseLineWhenOnArtist(line, ref state, ref currentAlbumPlayed, albums);
+                    break;
+
+            }
+        }
+
+        private static void ParseLineWhenOnArtist(
+            string line, 
+            ref FileReadState state,
+            ref AlbumPlayed currentAlbumPlayed,
+            List<AlbumPlayed> albums)
+        {
+            if (line[0] == '\t' && line[1] == '\t' && line[2] == '\t')
+            {
+                string album = line.TrimEnd(IgnoreChars).TrimStart(IgnoreChars);
+                currentAlbumPlayed.Album = album;
+                albums.Add(currentAlbumPlayed);
+                currentAlbumPlayed = new AlbumPlayed(currentAlbumPlayed);
+            }
+            else if (line[0] == '\t' && line[1] == '\t')
+            {
+                ParseLineWhenOnLocation(line, ref state, ref currentAlbumPlayed);
+            }
+            else if (line[0] == '\t')
+            {
+                ParseLineWhenOnDate(line, ref state, ref currentAlbumPlayed);
+
+            }
+            else
+            {
+                ParseLineWhenReset(line, ref state, ref currentAlbumPlayed);
+            }
+        }
+
+        private static void ParseLineWhenOnLocation(string line, ref FileReadState state, ref AlbumPlayed currentAlbumPlayed)
+        {
+            if (line[0] == '\t' && line[1] == '\t')
+            {
+                string artist = line.TrimEnd(IgnoreChars).TrimStart(IgnoreChars);
+                currentAlbumPlayed.Artist = artist;
+                state = FileReadState.OnArtist;
+            }
+        }
+
+        private static void ParseLineWhenOnDate(string line, ref FileReadState state, ref AlbumPlayed currentAlbumPlayed)
+        {
+            if (line[0] == '\t')
+            {
+                string location = line.TrimEnd(IgnoreChars).TrimStart(IgnoreChars);
+                currentAlbumPlayed.Location = location;
+                state = FileReadState.OnLocation;
+            }
+        }
+
+        private static void ParseLineWhenReset(string line, ref FileReadState state, ref AlbumPlayed currentAlbumPlayed)
+        {
+            if (char.IsLetterOrDigit(line[0]))
+            {
+                var words = line.Split(' ');
+                if (words.Length >= NumberWordsInDate)
+                {
+                    DateTime date = DateTime.Now;
+                    if (ParseDateFromString(words, ref date))
+                    {
+                        currentAlbumPlayed.Date = date;
+                        state = FileReadState.OnDate;
+                    }
+                }
+            }
+        }
+
+        private static bool ParseDateFromString(string[] words, ref DateTime date)
+        {
+            // example text would be "Monday 26th February 2018"
+            if (DayStrings.Contains(words[0].ToLower()))
+            {
+                var dayOfMonthString = words[1].ToLower();
+                var monthString = words[2].ToLower();
+                var yearString = words[3].ToLower();
+
+                int dayOfMonth = 1;
+                if (!EndsWithOrdinal(dayOfMonthString, ref dayOfMonth))
+                {
+                    return false;
+                }
+
+                if (!MonthStringsToNumbers.ContainsKey(monthString))
+                {
+                    return false;
+                }
+
+                int monthOfYear = MonthStringsToNumbers[monthString];
+
+                if (!int.TryParse(yearString, out var year) || year < 2015)
+                {
+                    return false;
+                }
+
+                date = new DateTime(year, monthOfYear, dayOfMonth, 12, 0, 0);
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool EndsWithOrdinal(string dayOfMonth, ref int day)
+        {
+            bool endsOk = false;
+            foreach (var ordinal in OrdinalStrings)
+            {
+                if(dayOfMonth.EndsWith(ordinal))
+                    endsOk = true;
+            }
+
+            if (endsOk)
+            {
+                string dayNumbers = dayOfMonth.Remove(dayOfMonth.Length - 2);
+                if (int.TryParse(dayNumbers, out day))
+                {
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public static List<AlbumPlayed> GetAlbumsPlayedFromFile(string filePath)
+        {
+            List<AlbumPlayed> albums = new List<AlbumPlayed>();
+
+            // check file exists
+            if (!File.Exists(filePath))
+                return albums;
+
+            // read the file line by line
+            string line;
+            FileReadState parseState = FileReadState.Reset;
+            AlbumPlayed currentAlbumPlayed  = new AlbumPlayed();
+
+            // Read the file and display it line by line.  
+            StreamReader file = new StreamReader(filePath);
+            while ((line = file.ReadLine()) != null)
+            {
+                ProcessLine(line, ref parseState, ref albums, ref currentAlbumPlayed);
+            }
+
+            file.Close();
+
+            return albums;
+        }
+
+        #endregion
+    }
+}
---
+++ SongsWebApp/Test/SongsFileImportExport.Test/Parser/SongFileParserTest.cs
@@ -0,0 +1,382 @@
+﻿using System.Linq;
+
+namespace SongsFileImportExport.Test.Parser
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using SongsDatabase.DataModels;
+
+    using SongsFileImportExport.Parser;
+
+    using TestUtilities;
+
+    [TestClass]
+    public class SongFileParserTest
+    {
+        #region Constants
+
+        public readonly string TestFilePathAllDays;
+
+        #endregion
+
+        #region Test utility Functions
+
+        private static void CheckAlbum_0(AlbumPlayed album)
+        {
+            Assert.IsNotNull(album);
+
+            // Check the date
+            Assert.AreEqual(2018, album.Date.Year);
+            Assert.AreEqual(1, album.Date.Month);
+            Assert.AreEqual(23, album.Date.Day);
+
+            // Check the location, artist & album
+            Assert.AreEqual("To Work", album.Location);
+            Assert.AreEqual("Current 93", album.Artist);
+            Assert.AreEqual("Halo", album.Album);
+
+            // check the other items are still blank
+            Assert.AreEqual(string.Empty, album.ImagePath);
+            Assert.AreEqual(string.Empty, album.PlayerLink);
+            Assert.AreEqual(string.Empty, album.UserName);
+        }
+
+        private static void CheckAlbum_1(AlbumPlayed album)
+        {
+            Assert.IsNotNull(album);
+
+            // Check the date
+            Assert.AreEqual(2018, album.Date.Year);
+            Assert.AreEqual(1, album.Date.Month);
+            Assert.AreEqual(23, album.Date.Day);
+
+            // Check the location, artist & album
+            Assert.AreEqual("In Work", album.Location);
+            Assert.AreEqual("Alice Coltrane", album.Artist);
+            Assert.AreEqual("World Spirituality Classics 1: The Ecstatic Music of Alice Coltrane Turiyasangitananda", album.Album);
+
+            // check the other items are still blank
+            Assert.AreEqual(string.Empty, album.ImagePath);
+            Assert.AreEqual(string.Empty, album.PlayerLink);
+            Assert.AreEqual(string.Empty, album.UserName);
+        }
+
+        private static void CheckAlbum_2(AlbumPlayed album)
+        {
+            Assert.IsNotNull(album);
+
+            // Check the date
+            Assert.AreEqual(2018, album.Date.Year);
+            Assert.AreEqual(1, album.Date.Month);
+            Assert.AreEqual(23, album.Date.Day);
+
+            // Check the location, artist & album
+            Assert.AreEqual("In Work", album.Location);
+            Assert.AreEqual("Lubomyr Melnyk", album.Artist);
+            Assert.AreEqual("Rivers And Streams", album.Album);
+
+            // check the other items are still blank
+            Assert.AreEqual(string.Empty, album.ImagePath);
+            Assert.AreEqual(string.Empty, album.PlayerLink);
+            Assert.AreEqual(string.Empty, album.UserName);
+        }
+
+
+        private static void CheckAlbum_3(AlbumPlayed album)
+        {
+            Assert.IsNotNull(album);
+
+            // Check the date
+            Assert.AreEqual(2018, album.Date.Year);
+            Assert.AreEqual(1, album.Date.Month);
+            Assert.AreEqual(24, album.Date.Day);
+
+            // Check the location, artist & album
+            Assert.AreEqual("To Work", album.Location);
+            Assert.AreEqual("Current 93", album.Artist);
+            Assert.AreEqual("Halo", album.Album);
+
+            // check the other items are still blank
+            Assert.AreEqual(string.Empty, album.ImagePath);
+            Assert.AreEqual(string.Empty, album.PlayerLink);
+            Assert.AreEqual(string.Empty, album.UserName);
+        }
+
+        private static void CheckAlbum_4(AlbumPlayed album)
+        {
+            Assert.IsNotNull(album);
+
+            // Check the date
+            Assert.AreEqual(2018, album.Date.Year);
+            Assert.AreEqual(1, album.Date.Month);
+            Assert.AreEqual(24, album.Date.Day);
+
+            // Check the location, artist & album
+            Assert.AreEqual("In Work", album.Location);
+            Assert.AreEqual("Nick Cave & the Bad Seeds", album.Artist);
+            Assert.AreEqual("Live from KCRW", album.Album);
+
+            // check the other items are still blank
+            Assert.AreEqual(string.Empty, album.ImagePath);
+            Assert.AreEqual(string.Empty, album.PlayerLink);
+            Assert.AreEqual(string.Empty, album.UserName);
+        }
+
+        private static void CheckAlbum_5(AlbumPlayed album)
+        {
+            Assert.IsNotNull(album);
+
+            // Check the date
+            Assert.AreEqual(2018, album.Date.Year);
+            Assert.AreEqual(1, album.Date.Month);
+            Assert.AreEqual(24, album.Date.Day);
+
+            // Check the location, artist & album
+            Assert.AreEqual("In Work", album.Location);
+            Assert.AreEqual("Acid Mothers Temple & The Melting Paraiso U.F.O.", album.Artist);
+            Assert.AreEqual("IAO Chant From the Melting Paraiso Underground Freak Out", album.Album);
+
+            // check the other items are still blank
+            Assert.AreEqual(string.Empty, album.ImagePath);
+            Assert.AreEqual(string.Empty, album.PlayerLink);
+            Assert.AreEqual(string.Empty, album.UserName);
+        }
+
+
+        private static void CheckAlbum_6(AlbumPlayed album)
+        {
+            Assert.IsNotNull(album);
+
+            // Check the date
+            Assert.AreEqual(2018, album.Date.Year);
+            Assert.AreEqual(1, album.Date.Month);
+            Assert.AreEqual(25, album.Date.Day);
+
+            // Check the location, artist & album
+            Assert.AreEqual("To Work", album.Location);
+            Assert.AreEqual("The Fall", album.Artist);
+            Assert.AreEqual("Palace of Swords Reversed", album.Album);
+
+            // check the other items are still blank
+            Assert.AreEqual(string.Empty, album.ImagePath);
+            Assert.AreEqual(string.Empty, album.PlayerLink);
+            Assert.AreEqual(string.Empty, album.UserName);
+        }
+
+        private static void CheckAlbum_7(AlbumPlayed album)
+        {
+            Assert.IsNotNull(album);
+
+            // Check the date
+            Assert.AreEqual(2018, album.Date.Year);
+            Assert.AreEqual(1, album.Date.Month);
+            Assert.AreEqual(25, album.Date.Day);
+
+            // Check the location, artist & album
+            Assert.AreEqual("In Work", album.Location);
+            Assert.AreEqual("The Fall", album.Artist);
+            Assert.AreEqual("Extricate", album.Album);
+
+            // check the other items are still blank
+            Assert.AreEqual(string.Empty, album.ImagePath);
+            Assert.AreEqual(string.Empty, album.PlayerLink);
+            Assert.AreEqual(string.Empty, album.UserName);
+        }
+
+        private static void CheckAlbum_8(AlbumPlayed album)
+        {
+            Assert.IsNotNull(album);
+
+            // Check the date
+            Assert.AreEqual(2018, album.Date.Year);
+            Assert.AreEqual(1, album.Date.Month);
+            Assert.AreEqual(25, album.Date.Day);
+
+            // Check the location, artist & album
+            Assert.AreEqual("In Work", album.Location);
+            Assert.AreEqual("The Fall", album.Artist);
+            Assert.AreEqual("Sub-Lingual Tablet", album.Album);
+
+            // check the other items are still blank
+            Assert.AreEqual(string.Empty, album.ImagePath);
+            Assert.AreEqual(string.Empty, album.PlayerLink);
+            Assert.AreEqual(string.Empty, album.UserName);
+        }
+
+
+        private static void CheckAlbum_Last(AlbumPlayed album)
+        {
+            Assert.IsNotNull(album);
+
+            // Check the date
+            Assert.AreEqual(2019, album.Date.Year);
+            Assert.AreEqual(8, album.Date.Month);
+            Assert.AreEqual(7, album.Date.Day);
+
+            // Check the location, artist & album
+            Assert.AreEqual("In Work", album.Location);
+            Assert.AreEqual("Trash Kit", album.Artist);
+            Assert.AreEqual("Confidence", album.Album);
+
+            // check the other items are still blank
+            Assert.AreEqual(string.Empty, album.ImagePath);
+            Assert.AreEqual(string.Empty, album.PlayerLink);
+            Assert.AreEqual(string.Empty, album.UserName);
+        }
+
+        #endregion
+
+        #region Tests
+
+        [TestMethod]
+        public void TestGetPathToFile()
+        {
+            Assert.IsTrue(File.Exists(TestFilePathAllDays));
+        }
+
+        [TestMethod]
+        public void TestInvalidFile()
+        {
+            Assert.IsTrue(File.Exists(TestFilePathAllDays));
+
+            string invalidFile = PathUtilities.TestFileNameAllDays + ".nothing";
+
+            Assert.IsFalse(File.Exists(invalidFile));
+
+            var albums = SongFileParser.GetAlbumsPlayedFromFile(invalidFile);
+            Assert.IsNotNull(albums);
+            Assert.AreEqual(0, albums.Count);
+        }
+
+        [TestMethod]
+        public void TestValidFile_SingleAlbumDay()
+        {
+            // Check the file is OK
+            string singleAlbumPath = PathUtilities.GetPathToFile(PathUtilities.TestFileNameSingleAlbum);
+            Assert.IsTrue(File.Exists(singleAlbumPath));
+
+            // Get the album
+            List<AlbumPlayed> albums = SongFileParser.GetAlbumsPlayedFromFile(singleAlbumPath);
+            Assert.IsNotNull(albums);
+            Assert.AreEqual(1, albums.Count);
+
+            // Check the first LP
+            CheckAlbum_0(albums[0]);
+        }
+
+        [TestMethod]
+        public void TestValidFile_TestFileNameSingleDay()
+        {
+            // Check the file is OK
+            string singleDayPath = PathUtilities.GetPathToFile(PathUtilities.TestFileNameSingleDay);
+            Assert.IsTrue(File.Exists(singleDayPath));
+
+            // Get the albums
+            List<AlbumPlayed> albums = SongFileParser.GetAlbumsPlayedFromFile(singleDayPath);
+            Assert.IsNotNull(albums);
+            Assert.AreEqual(2, albums.Count);
+
+            // Check the first LP
+            CheckAlbum_0(albums[0]);
+
+            // Check the second LP
+            CheckAlbum_1(albums[1]);
+        }
+
+        [TestMethod]
+        public void TestValidFile_TestFileNameMultipleSongsSingleDay()
+        {
+            // Check the file is OK
+            string singleDayPath = PathUtilities.GetPathToFile(PathUtilities.TestFileNameMultipleSongsSingleDay);
+            Assert.IsTrue(File.Exists(singleDayPath));
+
+            // Get the albums
+            List<AlbumPlayed> albums = SongFileParser.GetAlbumsPlayedFromFile(singleDayPath);
+            Assert.IsNotNull(albums);
+            Assert.AreEqual(3, albums.Count);
+
+            // Check the first LP
+            CheckAlbum_0(albums[0]);
+
+            // Check the second LP
+            CheckAlbum_1(albums[1]);
+
+            // Check the third LP
+            CheckAlbum_2(albums[2]);
+        }
+
+        [TestMethod]
+        public void TestValidFile_TestFileNameMultipleDaysSongs()
+        {
+            // Check the file is OK
+            string singleDayPath = PathUtilities.GetPathToFile(PathUtilities.TestFileNameMultipleDaysSongs);
+            Assert.IsTrue(File.Exists(singleDayPath));
+
+            // Get the albums
+            List<AlbumPlayed> albums = SongFileParser.GetAlbumsPlayedFromFile(singleDayPath);
+            Assert.IsNotNull(albums);
+            Assert.AreEqual(9, albums.Count);
+
+            // Check the first Day's LP
+            CheckAlbum_0(albums[0]);
+            CheckAlbum_1(albums[1]);
+            CheckAlbum_2(albums[2]);
+
+            // Check the second Day's LP
+            CheckAlbum_3(albums[3]);
+            CheckAlbum_4(albums[4]);
+            CheckAlbum_5(albums[5]);
+
+            // Check the third Day's LP
+            CheckAlbum_6(albums[6]);
+            CheckAlbum_7(albums[7]);
+            CheckAlbum_8(albums[8]);
+        }
+
+        [TestMethod]
+        public void TestValidFile_TestFileNameAllDays()
+        {
+            // Check the file is OK
+            string singleDayPath = PathUtilities.GetPathToFile(PathUtilities.TestFileNameAllDays);
+            Assert.IsTrue(File.Exists(singleDayPath));
+
+            // Get the albums
+            List<AlbumPlayed> albums = SongFileParser.GetAlbumsPlayedFromFile(singleDayPath);
+            Assert.IsNotNull(albums);
+            Assert.IsTrue(albums.Count > 900);
+
+            // Check the first Day's LP
+            CheckAlbum_0(albums[0]);
+            CheckAlbum_1(albums[1]);
+            CheckAlbum_2(albums[2]);
+
+            // Check the second Day's LP
+            CheckAlbum_3(albums[3]);
+            CheckAlbum_4(albums[4]);
+            CheckAlbum_5(albums[5]);
+
+            // Check the third Day's LP
+            CheckAlbum_6(albums[6]);
+            CheckAlbum_7(albums[7]);
+            CheckAlbum_8(albums[8]);
+
+
+            // Check the last LP
+            CheckAlbum_Last(albums.Last());
+
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public SongFileParserTest()
+        {
+            TestFilePathAllDays = PathUtilities.GetPathToFile(PathUtilities.TestFileNameAllDays);
+        }
+
+        #endregion
+    }
+}
